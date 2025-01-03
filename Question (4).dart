37a2844c972f3465de92012f106753b96e958c3c
UZAIR AHMED <uzairahmed175511@gmail.com>
2025-01-03 12:54:54 +0500

    [3, 9, 1, 6, 4, 2, 8, 5, 7], Output: Largest element: 9 using for loop

--- Question (4).dart
+++ Question (4).dart
@@ -1,6 +1,6 @@
 void main() {
   // Implement a code that finds the largest element in a list using a for loop.
-  // Example:Input: [3, 9, 1, 6, 4, 2, 8, 5, 7], Output: Largest element: 9
+  // Example:Input: [3, 9, 1, 6, 4, 2, 8, 5, 7], Output: Largest element: 9.
   List<int> num = [3, 9, 1, 6, 4, 2, 8, 5, 7];
   int largest_num = 0;
   for (int i = 0; i < num.length; i++) {
