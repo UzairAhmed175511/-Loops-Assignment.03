f38fd0a1b558bc026691c58c135dc885d837b7a4
UZAIR AHMED <uzairahmed175511@gmail.com>
2025-01-03 13:13:12 +0500

    list of numbers as input and prints the  numbers greater than 5 using a for loop

--- Question (15).dart
+++ Question (15).dart
@@ -6,10 +6,8 @@ void main() {
 
   print("Enter a list of numbers separated by spaces:");
   String? input = stdin.readLineSync();
-
   if (input != null && input.isNotEmpty) {
     List<int> numbers = input.split(' ').map(int.parse).toList();
-
     print("Numbers greater than 5:");
     for (int number in numbers) {
       if (number > 5) {
