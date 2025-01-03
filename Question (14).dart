4b40c1b663b97c7b901c9ab2ca7277de1d753f99
UZAIR AHMED <uzairahmed175511@gmail.com>
2025-01-03 13:12:13 +0500

    check email or password and login successfully

--- Question (14).dart
+++ Question (14).dart
@@ -5,8 +5,7 @@ void main() {
 // are given a list of predefined user credentials (email and password
 // combinations). If the entered email and password match any of the
 // credentials in the list, print "User login successful." Otherwise, keep
-// asking for the email and password until the correct credentials are
-// provided.
+// asking for the email and password until the correct credentials are provided.
 
   dynamic email = 'ua175511@gmail.com';
   dynamic password = 'qwertyuiop';
