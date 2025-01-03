8cf043e51566dc1efc4747ddc29799937406a902
UZAIR AHMED <uzairahmed175511@gmail.com>
2025-01-03 12:45:05 +0500

    factorial of a number using a while loop or for loop.

--- Question (2).dart
+++ Question (2).dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() {
   //(2)Implement a code that finds the factorial of a number using a while loop or for loop.
-  // Example:Input: 5 . Output: Factorial of 5 is 120
+  // Example:Input: 5 . Output: Factorial of 5 is 120.
   print('enter a nuber');
   int number = int.parse(stdin.readLineSync()!);
   int factorial = 1;
