589fcfae5c6d5fb0d2eac7092b3b790c0f4a4ea2
UZAIR AHMED <uzairahmed175511@gmail.com>
2025-01-03 13:03:02 +0500

    list of student details as input,print the student's name along with theirgrade.

--- Question (9).dart
+++ Question (9).dart
@@ -6,8 +6,7 @@ void main() {
 // List<Map<String, dynamic>> studentDetails = [
 // {'name': 'John', 'marks': [80, 75, 90], 'section': 'A', 'rollNumber': 101},
 // {'name': 'Emma', 'marks': [95, 92, 88], 'section': 'B', 'rollNumber': 102},
-// {'name': 'Ryan', 'marks': [70, 65, 75], 'section': 'A', 'rollNumber': 103},
-// ];
+// {'name': 'Ryan', 'marks': [70, 65, 75], 'section': 'A', 'rollNumber': 103},];
   List<Map<String, dynamic>> studentDetails = [
     {
       'name': 'John',
